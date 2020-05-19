ead58ee69d13e69eca392f56a0603295555b6940
Vasili Mandalac <vasilimandalac@Vasilis-MacBook-Pro.local>
2020-05-19 01:27:49 -0500

    initial

---
+++ docs/precache-manifest.c15e0515f80f4354115c008093137d2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8118a164f847d0a83766bcf19fa8ba4",
+    "url": "/luke-api-walker/index.html"
+  },
+  {
+    "revision": "4ba57d09ce0f8299f050",
+    "url": "/luke-api-walker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "988e2e33796f42308c34",
+    "url": "/luke-api-walker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ba57d09ce0f8299f050",
+    "url": "/luke-api-walker/static/js/2.07f7d806.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/luke-api-walker/static/js/2.07f7d806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988e2e33796f42308c34",
+    "url": "/luke-api-walker/static/js/main.452bff7f.chunk.js"
+  },
+  {
+    "revision": "18d9522d3867d4ef6323",
+    "url": "/luke-api-walker/static/js/runtime-main.09d58874.js"
+  }
+]);
